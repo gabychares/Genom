e42e82d203ce19dc85a69607cee4a8d69a761c31
gabychares <gchavez27@alumnos.uaq.mx>
2026-02-25 10:29:14 -0600

    agregué líneas de código

---
+++ scripts/crear_repositorio_github.R
@@ -0,0 +1,15 @@
+#primero se genera el project, puede ser uno nuevo o uno existente pero que lo quieres convertir en un repositorio de github
+#ESTO SOLO SE HACE UNA VEZ POR MÁQUINA
+#crear un token de github:
+usethis::create_github_token()
+#credenciales y aquí es donde te pide el token
+gitcreds::gitcreds_set()
+
+#Configurar
+usethis::use_git_config(
+  user.name = "gabychares",
+  user.email = "gchavez27@alumnos.uaq.mx"
+)
+
+#subir el repositorio al github
+usethis::use_github()
